23f82127ccedf69d90cc75baa1cef40c0b2e59b9
admin <krylenko.fcul@gmail.com>
2019-09-18 17:08:24 +0100

    depois do exists

---
+++ initial/src/main/java/hello/CountryEndpoint.java
@@ -0,0 +1,44 @@
+
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.ExistsCountryRequest;
+import io.spring.guides.gs_producing_web_service.ExistsCountryResponse;
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+	// @Autowired
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "existsCountryRequest")
+	@ResponsePayload
+	public ExistsCountryResponse existCountry(@RequestPayload ExistsCountryRequest request) {
+		ExistsCountryResponse response = new ExistsCountryResponse();
+		response.setExists(countryRepository.existsCountry(request.getName()));
+		return response;
+
+	}
+
+}
